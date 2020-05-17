1f472b2111aedb80ea5ae89f6c73759de11738b6
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-17 03:04:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(current_card_total)
   prompt_user
   decision = get_user_input
     if decision == "h"
-      next_card = deal_card
+      deal_card 
       new_total = next_card + current_card_total
   ``elsif decision == "s" 
       new_total = current_card_total
